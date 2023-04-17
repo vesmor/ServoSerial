a94797c08999ec85b25675dc934fc8a197433dc9
vesmor <romsev.charles@gmail.com>
2023-04-17 18:43:49 -0400

    changed servo library with servotimer2

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,28 +1,45 @@
-#include "Servo.h"
+#include "ServoTimer2.h"
 #include <SoftwareSerial.h>
 #define S_PIN 9
 #define T_PIN 10
 
-#define BAUD_RATE 9600
+#define BAUD_RATE 115200
 #define RX_PIN 2
 #define TX_PIN 3
 
 SoftwareSerial senSerial(RX_PIN, TX_PIN);
 
-Servo s;
-Servo t;
+ServoTimer2 s;
+ServoTimer2 t;
+
 int angle = 0;
 
 void setup() {
   senSerial.begin(19200);
+  Serial.begin(BAUD_RATE);
   s.attach(S_PIN);
   t.attach(T_PIN);
+  s.write(angle);
+  Serial.println("Receiver ready!");
 }
 
 void loop() {
-  while (!senSerial.available()) {
+  // while (!senSerial.available()) {
     
-    s.write(angle);
+  //   s.write(angle + 750);
+  //   delay(100);
+  // }
+  // angle = senSerial.read();
+  // delay(100);
+  while(!senSerial.available()){
+
   }
-  angle = senSerial.read();
+  int oldangle = senSerial.parseInt();
+  angle = map(oldangle, 0 , 180, 750, 2250); //because the servo2 library maps to microS not angle
+  s.write(angle);
+
+  Serial.print("angle: ");
+  Serial.println(oldangle);
+
+  delay(100);
 }
