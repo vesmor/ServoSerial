03b64c0615d27cbf11ca0bb6e6cdfdbf0140364e
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2023-04-20 21:55:24 -0400

    Dependent on which microcont. powered first

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -4,39 +4,39 @@
 
 #define BAUD_RATE 115200
 #define RX_PIN 7
-#define OPEN 0
-#define CLOSE 90
+#define OPEN 90
+#define CLOSE 0
 
 Servo s;
 Servo t;
-
-int angle = 0;
 
 void setup()
 {
   Serial.begin(BAUD_RATE);
   pinMode(RX_PIN, INPUT_PULLUP);
-
   s.attach(S_PIN);
+  s.write(CLOSE);
   t.attach(T_PIN);
-  s.write(OPEN);
+  //Initialization If reciever is powered first Otherwise the delay should be fine
+  // if(digitalRead(RX_PIN) == LOW){
+  // while(digitalRead(RX_PIN) == LOW); //its gonna be LOW becuase theres no power for a bit
+  // while(digitalRead(RX_PIN) == HIGH);//PINS are all HIGH when initialized so we have to wait for them to not be
+  // }
+  // OR we could just wait lol
   delay(1000);
   Serial.println("Receiver ready!");
 }
 
 void loop()
 {
-
-  if (digitalRead(RX_PIN) == LOW)
-  {
-    Serial.println("LOW");
-    s.write(CLOSE);
-  }
   if (digitalRead(RX_PIN) == HIGH)
   {
     Serial.println("HIGH");
+    s.write(CLOSE);
+  }
+  if (digitalRead(RX_PIN) == LOW)
+  {
+    Serial.println("LOW");
     s.write(OPEN);
   }
-
-  delay(100);
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -4,32 +4,22 @@
 #define BAUD_RATE 115200
 #define TX_PIN 3
 
-#define CLOSE 0
 #define OPEN 1
+#define CLOSE 0
 
-int mode = 0;
+int mode = CLOSE;
 
 void setup()
 {
   Serial.begin(BAUD_RATE);
-
   pinMode(TX_PIN, OUTPUT);
+  digitalWrite(TX_PIN, HIGH);
+  delay(1000);
 }
 
 // send random angle between 0 and 180 to receiver with servo attached
 void loop()
 {
-  // //Send
-  // Serial.println("Enter an angle from 0-180:");
-  // //Wait for user input
-  // while (!Serial.available()) {  }
-  // C = Serial.parseInt();
-  // Serial.flush();
-  // //In case we dont get the right values
-  // if(C==0)recSerial.print(CLOSE);
-  // if(C==1)recSerial.print(OPEN);
-  // Serial.print(angle);
-  // delay(100);
 
   Serial.println("Enter 0. Close or 1. Open:");
 
@@ -38,18 +28,17 @@ void loop()
   }
 
   mode = Serial.parseInt();
+  Serial.println(mode);
   if (mode == OPEN)
   {
-    mode = OPEN;
-    digitalWrite(TX_PIN, HIGH);
+    mode = CLOSE;
+    digitalWrite(TX_PIN, LOW);
     Serial.println("Opening to 90 degrees");
   }
   else if (mode == CLOSE)
   {
     mode = CLOSE;
-    digitalWrite(TX_PIN, LOW);
+    digitalWrite(TX_PIN, HIGH);
     Serial.println("Closing to 0 degrees");
   }
-
-  delay(50);
 }
